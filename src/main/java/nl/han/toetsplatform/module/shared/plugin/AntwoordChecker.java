abe7fae0d855e994d067f361cd0ecd805024cb4b
Danny Zwart <d.zwart@student.han.nl>
2018-06-06 09:02:50 +0200

    Added mendatory api functions following the development view

--- src/main/java/nl/han/toetsplatform/module/shared/plugin/AntwoordChecker.java
+++ src/main/java/nl/han/toetsplatform/module/shared/plugin/AntwoordChecker.java
@@ -3,5 +3,8 @@ package nl.han.toetsplatform.module.shared.plugin;
 public interface AntwoordChecker {
 
     int getPunten(String gegevenAntwoord, String goedAntwoord);
+    String getCheckedAntwoord();
+    boolean hasCheckedAntwoord();
+    boolean checkAntwoord(String gegevenAntwoord, String goedAntwoord);
 
 }
