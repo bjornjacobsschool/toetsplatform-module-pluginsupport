0f5071b5f467c87711756d08462bd5b265d53337
Erik koning <EW.deKoning@student.han.nl>
2018-06-08 10:31:14 +0200

    NakijkenView hernoemdt naar NakijkView gezorcht dat in de nakijkview de javafx Node class werdt gebruikt inplaats van de Soap Node

--- src/main/java/nl/han/toetsplatform/module/shared/plugin/NakijkView.java
+++ src/main/java/nl/han/toetsplatform/module/shared/plugin/NakijkView.java
@@ -1,14 +1,15 @@
 package nl.han.toetsplatform.module.shared.plugin;
 
+import javafx.scene.Node;
 import nl.han.toetsplatform.module.shared.plugin.exceptions.NietBerekenbaarException;
 
-import javax.xml.soap.Node;
+
 
 /**
  * Is verantwoordelijk voor het generen van een view waar een vraag nagekeken kan worden met eventueel de antwoordchecker
  * Maakt gebruik van antwoordchecker
  */
-public interface NakijkenView {
+public interface NakijkView {
 
     Node getView();
 
