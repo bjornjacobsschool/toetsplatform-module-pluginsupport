8b7adba26a8ef96bbb8eab4c82944f7413a133f1
Cesco van Zundert <cl.vanzundert@student.han.nl>
2018-06-11 14:11:21 +0200

    Reimplement maxpunten

--- src/main/java/nl/han/toetsplatform/module/shared/plugin/NakijkView.java
+++ src/main/java/nl/han/toetsplatform/module/shared/plugin/NakijkView.java
@@ -12,4 +12,11 @@ import nl.han.toetsplatform.module.shared.plugin.exceptions.NietBerekenbaarExcep
 public interface NakijkView {
 
     Node getView();
+
+    /**
+     * Berekend de behaalbare punten
+     * @return Het aantal punten dat de plugin bepaald heeft
+     * @throws NietBerekenbaarException wanneer de plugin niet in staat is om het aantal punten
+     */
+    int getBerekendePunten() throws NietBerekenbaarException;
 }
